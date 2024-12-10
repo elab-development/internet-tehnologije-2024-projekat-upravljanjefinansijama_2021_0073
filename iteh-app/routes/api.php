4928e28fd45e767f1c58b9eb788b10a5052538df
ZigiStardast <129152646+ZigiStardast@users.noreply.github.com>
2024-12-10 18:43:32 +0100

    dodate neke api rute

--- iteh-app/routes/api.php
+++ iteh-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\BudgetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('/budgets',BudgetController::class);
+
+    Route::post('/logout', [AuthController::class,'logout']);
 });
